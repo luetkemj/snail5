bc16d66f6782deff603c4fdee3401a9e4da04a1c
luetkemj <luetkemj@gmail.com>
2020-03-14 13:58:19 -0500

    feat: breadthfirst early break

--- src/lib/pathfinding.js
+++ src/lib/pathfinding.js
@@ -32,7 +32,7 @@ export const floodFill = startLoc => {
   return visited;
 };
 
-export const breadthFirst = startLoc => {
+export const breadthFirst = (startLoc, goalLoc) => {
   const frontier = [startLoc];
   const cameFrom = {};
 
@@ -41,6 +41,7 @@ export const breadthFirst = startLoc => {
 
     const currentCell = idToCell(current);
     const neighbors = getNeighborIds(currentCell.x, currentCell.y);
+    let complete = false;
 
     neighbors.forEach(nLocId => {
       if (
@@ -53,13 +54,19 @@ export const breadthFirst = startLoc => {
         frontier.unshift(nLocId);
         cameFrom[nLocId] = current;
 
-        ECS.cache.entitiesAtLocation[nLocId].forEach(id => {
-          const direction = getDirection(current, nLocId);
-          getEntity(id).components.breadthFirst = direction;
-          getEntity(id).components.cameFrom = current;
-        });
+        ECS.cache.entitiesAtLocation[nLocId] &&
+          ECS.cache.entitiesAtLocation[nLocId].forEach(id => {
+            const direction = getDirection(current, nLocId);
+            getEntity(id).components.breadthFirst = direction;
+            getEntity(id).components.cameFrom = current;
+          });
+        // else if this is true we have reached our goal and can stop the search
+      } else if (goalLoc && nLocId === goalLoc) {
+        complete = true;
       }
     });
+
+    if (complete) break;
   }
 
   return cameFrom;
